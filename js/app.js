7b084ae09c0b85856474c78c9996bc15522d4faf
Rafael Römhild <rafael@roemhild.de>
2019-09-23 09:22:32 +0200

    Reformat with prettier

--- js/app.js
+++ js/app.js
@@ -9,10 +9,10 @@
 var devices = new Vue({
   el: "#device",
   data: {
-    deviceList: {},
+    deviceList: {}
   },
   methods: {
-    badge_appearance: function (state) {
+    badge_appearance: function(state) {
       if (state === "init") {
         return "badge-info";
       } else if (state === "ready") {
@@ -21,36 +21,34 @@ var devices = new Vue({
         return "badge-warning";
       } else if (state === "dicsonnected") {
         return "badge-secondary";
-      }
-      else if (state === "sleeping") {
+      } else if (state === "sleeping") {
         return "badge-primary";
       } else if (state === "alert") {
         return "badge-danger";
       }
-
     },
-    timeSince: function (secs) {
-      var t = new Date(1970,0,1);
+    timeSince: function(secs) {
+      var t = new Date(1970, 0, 1);
       t.setSeconds(secs);
-      var s = t.toTimeString().substr(0,8);
-      if(secs > 86399)
-          s = Math.floor((t - Date.parse("1/1/70")) / 3600000) + s.substr(2);
+      var s = t.toTimeString().substr(0, 8);
+      if (secs > 86399)
+        s = Math.floor((t - Date.parse("1/1/70")) / 3600000) + s.substr(2);
       return s;
     },
     changeObjectItem: function() {
-            this.$set(this.updated, true);
-    },
+      this.$set(this.updated, true);
+    }
   }
-})
+});
 
 var client_status = new Vue({
   el: "#client_status",
   data: {
-   connected: false,
-   message: "dicsonnected",
+    connected: false,
+    message: "dicsonnected"
   },
   methods: {
-    badge_appearance: function (state) {
+    badge_appearance: function(state) {
       if (this.connected === true) {
         return "badge-success";
       } else {
@@ -58,11 +56,14 @@ var client_status = new Vue({
       }
     }
   }
-})
-
+});
 
 // MQTT Client
-var clientId = "hodd_" + Math.random().toString(16).substr(2, 8)
+var clientId =
+  "hodd_" +
+  Math.random()
+    .toString(16)
+    .substr(2, 8);
 client = new Paho.Client(MQTT_BROKER, Number(MQTT_PORT), clientId);
 
 // set callback handlers
@@ -75,32 +76,32 @@ var options = {
   onSuccess: onConnect,
   onFailure: doFail,
   reconnect: true,
-  timeout: 10,
-}
+  timeout: 10
+};
 
 // connect the client
 client.connect(options);
 
-function doFail(e){
+function doFail(e) {
   console.log(e);
   client_status.connected = false;
-  client_status.message = "dicsonnected"
+  client_status.message = "dicsonnected";
 }
 
 // called when the client connects
 function onConnect() {
   client_status.connected = true;
-  client_status.message = "connected"
+  client_status.message = "connected";
   client.subscribe(DISCOVERY_TOPIC);
 }
 
 // called when the client looses its connection
 function onConnectionLost(responseObject) {
   if (responseObject.errorCode !== 0) {
-      console.log("onConnectionLost:"+responseObject.errorMessage);
+    console.log("onConnectionLost:" + responseObject.errorMessage);
   }
   client_status.connected = false;
-  client_status.message = "lost"
+  client_status.message = "lost";
 }
 
 // called when a message arrives
@@ -126,8 +127,8 @@ function onMessageArrived(message) {
       stats_uptime: "",
       stats_freeheap: "",
       implementation: "",
-      nodes: {},
-    })
+      nodes: {}
+    });
 
     // subscribe to device topics
     client.subscribe(topic[0] + "/" + topic[1] + "/+");
@@ -137,105 +138,120 @@ function onMessageArrived(message) {
     // process attributes and payloads
     if (topic[2] === "$name") {
       devices.deviceList[device_id]["name"] = payload;
-    }
-    else if (topic[2] === "$state") {
+    } else if (topic[2] === "$state") {
       devices.deviceList[device_id]["state"] = payload;
-    }
-    else if (topic[2] ==="$localip") {
+    } else if (topic[2] === "$localip") {
       devices.deviceList[device_id]["localip"] = payload;
-    }
-    else if (topic[2] === "$mac") {
+    } else if (topic[2] === "$mac") {
       devices.deviceList[device_id]["mac"] = payload;
-    }
-    else if (topic[2] === "$implementation") {
+    } else if (topic[2] === "$implementation") {
       devices.deviceList[device_id]["implementation"] = payload;
-    }
-    else if (topic[2] === "$fw") {
+    } else if (topic[2] === "$fw") {
       if (topic[3] === "name") {
         devices.deviceList[device_id]["fw_name"] = payload;
       } else if (topic[3] === "version") {
-          devices.deviceList[device_id]["fw_version"] = payload;
+        devices.deviceList[device_id]["fw_version"] = payload;
       }
-    }
-    else if (topic[2] === "$stats") {
+    } else if (topic[2] === "$stats") {
       if (topic[3] === "uptime") {
         devices.deviceList[device_id]["stats_uptime"] = payload;
-      }
-      else if (topic[3] === "interval") {
+      } else if (topic[3] === "interval") {
         devices.deviceList[device_id]["stats_interval"] = payload;
-      }
-      else if (topic[3] === "freeheap") {
+      } else if (topic[3] === "freeheap") {
         devices.deviceList[device_id]["stats_freeheap"] = payload;
       }
-    }
-    else if (topic[2] === "$nodes") {
+    } else if (topic[2] === "$nodes") {
       // add nodes to device object
-      var nodes = payload.split(",")
+      var nodes = payload.split(",");
       for (n in nodes) {
         // devices.$set(devices.deviceList.nodes, node, {});
         devices.$set(devices.deviceList[device_id].nodes, nodes[n], {
           id: nodes[n],
           name: "",
           type: "",
-          properties: {},
+          properties: {}
         });
         client.subscribe(topic[0] + "/" + topic[1] + "/" + nodes[n] + "/$name");
         client.subscribe(topic[0] + "/" + topic[1] + "/" + nodes[n] + "/$type");
-        client.subscribe(topic[0] + "/" + topic[1] + "/" + nodes[n] + "/$properties");
+        client.subscribe(
+          topic[0] + "/" + topic[1] + "/" + nodes[n] + "/$properties"
+        );
       }
-    }
-    else if (topic[2] in devices.deviceList[device_id]["nodes"]) {
+    } else if (topic[2] in devices.deviceList[device_id]["nodes"]) {
       // add attributes to node
       var node = topic[2];
 
       if (topic[3] === "$name") {
         devices.deviceList[device_id]["nodes"][node]["name"] = payload;
-      }
-      else if (topic[3] === "$type") {
+      } else if (topic[3] === "$type") {
         devices.deviceList[device_id]["nodes"][node]["type"] = payload;
-      }
-      else if (topic[3] === "$properties") {
-        var properties = payload.split(",")
+      } else if (topic[3] === "$properties") {
+        var properties = payload.split(",");
         for (p in properties) {
-          devices.$set(devices.deviceList[device_id].nodes[node].properties, properties[p], {
-            id: properties[p],
-            name: "",
-            settable: "false",
-            datatype: "string",
-            format: "",
-            retained: "true",
-            unit: "",
-            state: "",
-            topic: "",
-          });
-          client.subscribe(topic[0] + "/" + topic[1] + "/" + topic[2] + "/" + properties[p])
-          client.subscribe(topic[0] + "/" + topic[1] + "/" + topic[2] + "/" + properties[p] + "/+");
+          devices.$set(
+            devices.deviceList[device_id].nodes[node].properties,
+            properties[p],
+            {
+              id: properties[p],
+              name: "",
+              settable: "false",
+              datatype: "string",
+              format: "",
+              retained: "true",
+              unit: "",
+              state: "",
+              topic: ""
+            }
+          );
+          client.subscribe(
+            topic[0] + "/" + topic[1] + "/" + topic[2] + "/" + properties[p]
+          );
+          client.subscribe(
+            topic[0] +
+              "/" +
+              topic[1] +
+              "/" +
+              topic[2] +
+              "/" +
+              properties[p] +
+              "/+"
+          );
         }
-      }
-      else if (topic[3] in devices.deviceList[device_id]["nodes"][node]["properties"]) {
-        property = topic[3]
+      } else if (
+        topic[3] in devices.deviceList[device_id]["nodes"][node]["properties"]
+      ) {
+        property = topic[3];
         if (topic.length === 4) {
-          devices.deviceList[device_id]["nodes"][node]["properties"][property]["state"] = payload;
-          devices.deviceList[device_id]["nodes"][node]["properties"][property]["topic"] = message.destinationName
-        }
-        else if (topic[4] === "$name") {
-          devices.deviceList[device_id]["nodes"][node]["properties"][property]["name"] = payload;
-
-        }
-        else if (topic[4] === "$settable") {
-          devices.deviceList[device_id]["nodes"][node]["properties"][property]["settable"] = payload;
-        }
-        else if (topic[4] === "$datatype") {
-          devices.deviceList[device_id]["nodes"][node]["properties"][property]["datatype"] = payload;
-        }
-        else if (topic[4] === "$format") {
-          devices.deviceList[device_id]["nodes"][node]["properties"][property]["format"] = payload;
-        }
-        else if (topic[4] === "$retained") {
-          devices.deviceList[device_id]["nodes"][node]["properties"][property]["retained"] = payload;
-        }
-        else if (topic[4] === "$unit") {
-          devices.deviceList[device_id]["nodes"][node]["properties"][property]["unit"] = payload;
+          devices.deviceList[device_id]["nodes"][node]["properties"][property][
+            "state"
+          ] = payload;
+          devices.deviceList[device_id]["nodes"][node]["properties"][property][
+            "topic"
+          ] = message.destinationName;
+        } else if (topic[4] === "$name") {
+          devices.deviceList[device_id]["nodes"][node]["properties"][property][
+            "name"
+          ] = payload;
+        } else if (topic[4] === "$settable") {
+          devices.deviceList[device_id]["nodes"][node]["properties"][property][
+            "settable"
+          ] = payload;
+        } else if (topic[4] === "$datatype") {
+          devices.deviceList[device_id]["nodes"][node]["properties"][property][
+            "datatype"
+          ] = payload;
+        } else if (topic[4] === "$format") {
+          devices.deviceList[device_id]["nodes"][node]["properties"][property][
+            "format"
+          ] = payload;
+        } else if (topic[4] === "$retained") {
+          devices.deviceList[device_id]["nodes"][node]["properties"][property][
+            "retained"
+          ] = payload;
+        } else if (topic[4] === "$unit") {
+          devices.deviceList[device_id]["nodes"][node]["properties"][property][
+            "unit"
+          ] = payload;
         }
       }
     }
--- settings.example.js
+++ settings.example.js
@@ -2,16 +2,15 @@
  *  MQTT Settings
  */
 
-var MQTT_BROKER = "10.0.0.1"
-var MQTT_USER = "homie"
-var MQTT_PASS = "secret"
-var MQTT_PORT = 8084
-var MQTT_USE_SSL = true
-
+var MQTT_BROKER = "10.0.0.1";
+var MQTT_USER = "homie";
+var MQTT_PASS = "secret";
+var MQTT_PORT = 8084;
+var MQTT_USE_SSL = true;
 
 /*
  *  Homie discovery topic
  */
 
 // var DISCOVERY_TOPIC = "homie/+/$homie"
-var DISCOVERY_TOPIC = "+/+/$homie"
+var DISCOVERY_TOPIC = "+/+/$homie";
